29343f55ceb6aec4d49baac70fc75aa22ec240e6
Njuguna <engbrian01@gmail.com>
2025-05-27 10:55:19 +0300

    added task table with id,title,description,completed status

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,2 +1,10 @@
 import sqlite3
 from .user import User
+from .task import Task
+
+DB_FILE = "todo.db"
+
+def create_tables():
+    """Create database tables"""
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
