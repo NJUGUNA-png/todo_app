5c2c7da7113e4ad4c60c10ff13c9520b398fda8e
Njuguna <engbrian01@gmail.com>
2025-05-27 18:57:38 +0300

    added query method find_by_id()

--- lib/models/task.py
+++ lib/models/task.py
@@ -10,34 +10,53 @@ class Task:
         self.user_id = user_id
 
 def save(self):
-        conn = sqlite3.connect(DB_FILE)
-        cursor = conn.cursor()
-        
-        if self.id:
-            cursor.execute(
-                """UPDATE tasks SET title=?, description=?, completed=?, user_id=?
-                WHERE id=?""",
-                (self.title, self.description, self.completed, self.user_id, self.id)
-            )
-        else:
-            cursor.execute(
-                """INSERT INTO tasks (title, description, completed, user_id)
-                VALUES (?, ?, ?, ?)""",
-                (self.title, self.description, self.completed, self.user_id)
-            )
-            self.id = cursor.lastrowid
-        
-        conn.commit()
-        conn.close()
-        return self
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    
+    if self.id:
+        cursor.execute(
+            """UPDATE tasks SET title=?, description=?, completed=?, user_id=?
+            WHERE id=?""",
+            (self.title, self.description, self.completed, self.user_id, self.id)
+        )
+    else:
+        cursor.execute(
+            """INSERT INTO tasks (title, description, completed, user_id)
+            VALUES (?, ?, ?, ?)""",
+            (self.title, self.description, self.completed, self.user_id)
+        )
+        self.id = cursor.lastrowid
+    
+    conn.commit()
+    conn.close()
+    return self
 
 def delete(self):
-        conn = sqlite3.connect(DB_FILE)
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM tasks WHERE id=?", (self.id,))
-        conn.commit()
-        conn.close()
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM tasks WHERE id=?", (self.id,))
+    conn.commit()
+    conn.close()
 
 def toggle_complete(self):
-        self.completed = not self.completed
-        self.save()
+    self.completed = not self.completed
+    self.save()
+    
+@staticmethod
+def find_by_id(task_id):
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks WHERE id=?", (task_id,))
+    row = cursor.fetchone()
+    conn.close()
+    
+    if row:
+        return Task(
+            id=row[0],
+            title=row[1],
+            description=row[2],
+            completed=bool(row[3]),
+            user_id=row[4]
+        )
+    return None
+
