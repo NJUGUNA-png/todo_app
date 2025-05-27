9a4e7747cbc0d4f380e58869693143695dda0bfb
Njuguna <engbrian01@gmail.com>
2025-05-27 11:02:57 +0300

    added save()method for create and update operations

--- lib/models/user.py
+++ lib/models/user.py
@@ -6,3 +6,23 @@ class User:
         self.id = id
         self.username = username
         self.email = email
+
+        def save(self):
+        conn = sqlite3.connect(DB_FILE)
+        cursor = conn.cursor()
+        
+        if self.id:
+            cursor.execute(
+                "UPDATE users SET username=?, email=? WHERE id=?",
+                (self.username, self.email, self.id)
+            )
+        else:
+            cursor.execute(
+                "INSERT INTO users (username, email) VALUES (?, ?)",
+                (self.username, self.email)
+            )
+            self.id = cursor.lastrowid
+        
+        conn.commit()
+        conn.close()
+        return self
