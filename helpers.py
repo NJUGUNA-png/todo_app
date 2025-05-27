5195ed913eac7e5ff6d3ad00e479560ab3ee0881
Njuguna <engbrian01@gmail.com>
2025-05-27 22:02:54 +0300

    implemented task operation create_task

--- helpers.py
+++ helpers.py
@@ -14,3 +14,8 @@ def get_all_users():
 def find_user_by_id(user_id):
     """Find user by ID"""
     return User.find_by_id(user_id)
+
+def create_task(title, description, user_id):
+    """Create a new task"""
+    task = Task(title=title, description=description, user_id=user_id)
+    return task.save()
