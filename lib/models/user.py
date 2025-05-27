f2b0ada853ecf26ddaf012f635780cfdbb90d9dd
Njuguna <engbrian01@gmail.com>
2025-05-27 11:01:29 +0300

    implemented user model with id,username and email attributes

--- lib/models/user.py
+++ lib/models/user.py
@@ -1,2 +1,8 @@
 import sqlite3
 from lib.models import DB_FILE
+
+class User:
+    def __init__(self, username, email, id=None):
+        self.id = id
+        self.username = username
+        self.email = email
